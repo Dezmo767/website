3f00c16623209143f1f99068c5b91f4e082affdb
Dezmo767 <36622527+Dezmo767@users.noreply.github.com>
2024-04-28 12:05:16 +0200

    Website Version 0.3
    Project Pages and Carousel added fixed some alignment problems

--- script/script.js
+++ script/script.js
@@ -4,3 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var parallaxElems = document.querySelectorAll('.parallax');
     var parallaxInstances = M.Parallax.init(parallaxElems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {});
+  });
